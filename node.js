79ca3b1ae2e00c63449a8e9b8f87997321798a5f
ryemaan <dhdjbzbsdv@gmail.com>
2026-04-30 15:03:12 +0500

    Update node.js

--- node.js
+++ node.js
@@ -6,7 +6,6 @@ const hostname = '192.168.10.254';
 const port = 3001;
 
 const server = http.createServer((req, res) => {
-  // Читаем файл index.html и отправляем его клиенту
   fs.readFile('index.html', (err, data) => {
     if (err) {
       res.statusCode = 500;
@@ -20,21 +19,15 @@ const server = http.createServer((req, res) => {
   });
 });
 
-// Запускаем сервер
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
   
-  // Автоматически открываем браузер (на Windows, Linux или Mac)
   const url = `http://${hostname}:${port}/`;
   // Для Windows
   if (process.platform === 'win32') {
     exec(`start ${url}`);
   } 
-  // Для MacOS
-  else if (process.platform === 'darwin') {
-    exec(`open ${url}`);
   } 
-  // Для Linux
   else {
     exec(`xdg-open ${url}`);
   }
